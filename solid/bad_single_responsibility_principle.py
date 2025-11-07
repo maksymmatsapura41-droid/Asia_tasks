35c5262dc75ba81b106cfb05123cc63110b051a5
Maksym Matsapura <mmatsapura2905@gmail.com>
2025-11-07 17:24:23 +0200

    Add snippsets of code 07_11

---
+++ solid/bad_single_responsibility_principle.py
@@ -0,0 +1,15 @@
+class Baker:
+    def bake_bread(self):
+        print("Baking bread...")
+
+    def manage_inventory(self):
+        print("Checking flour and yeast...")
+
+    def order_supplies(self):
+        print("Ordering more ingredients...")
+
+    def sell_bread(self):
+        print("Selling bread to customers...")
+
+    def clean_bakery(self):
+        print("Cleaning the bakery...")
