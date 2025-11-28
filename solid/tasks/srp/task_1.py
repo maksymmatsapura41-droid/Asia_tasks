1d3371f73a7ad3f55069c543d52d866118ca52c7
netwgl <netwgl@gmail.com>
2025-11-28 16:30:30 +0100

    task1

--- solid/tasks/srp/task_1.py
+++ solid/tasks/srp/task_1.py
@@ -4,11 +4,32 @@ class Movie:
         self.rating = rating
         self.duration = duration
 
-    def play(self):
-        print(f"Playing {self.title}")
+class MoviePlay:
+    @staticmethod
+    def play(movie):
+        print(f"Playing {movie.title}")
 
-    def save_to_db(self):
-        pass
+class MovieSaver:
+    @staticmethod
+    def save_to_db(movie):
+        print(f'saving {movie.title} to local storage')
 
-    def send_notification(self):
-        pass
+class MovieNotification:
+    @staticmethod
+    def send_notification(movie):
+        print(f'{movie.title} is released')
+
+class MovieManager(MovieSaver, MovieNotification):
+    def __init__(self):
+        self.movie_player = MoviePlay()
+
+
+movie1 = Movie('Movie1', 8.0, 90)
+
+movie_manager = MovieManager()
+# movie_manager.save_to_db(movie1)
+# movie_manager.send_notification(movie1)
+movie_manager.movie_player.play(movie1)
+
+    
+
