e34d7bf371430bf30d213a8353a84972ff11dc2f
netwgl <netwgl@gmail.com>
2025-12-04 17:41:49 +0100

    h/w 28_11 work in process

--- homework/28_11/ocp_task.py
+++ homework/28_11/ocp_task.py
@@ -5,14 +5,44 @@
 # Добавить новый канал TelegramChannel без изменения существующих классов и показать, что он работает.
 
 
+from abc import ABC, abstractmethod
+from dataclasses import dataclass, field
+
+class MediaChannel(ABC):
+    @abstractmethod
+    def notify(self, message):
+        raise NotImplementedError("Method area should be implemented")
+
+class EmailChannel:
+    def __init__(self):
+        pass
+
+    def notify(self, message):
+        print(f"Email: {message}")
+        
+class SMSChannel:
+    def __init__(self):
+        pass
+
+    def notify(self, message):
+        print(f"SMS: {message}")
+
+
+class TelegramChannel:
+    def __init__(self):
+        pass
+
+    def notify(self, message):
+        print(f"Telegram: {message}")
+
+@dataclass
 class NotificationManager:
-    def notify(self, type, message):
-        if type == "email":
-            print(f"Email: {message}")
-        elif type == "sms":
-            print(f"SMS: {message}")
+    channels: list = field(default_factory=list)
+
+    def notify(self, message):
+        for channel in self.channels:
+            channel.notify(message)
 
 
-manager = NotificationManager()
-manager.notify("email", "Your order has been shipped")
-manager.notify("sms", "Your order has been shipped")
+manager = NotificationManager([EmailChannel(), SMSChannel(), TelegramChannel()])
+manager.notify("Your order has been shipped")
--- homework/28_11/srp_task.py
+++ homework/28_11/srp_task.py
@@ -11,14 +11,42 @@ class BankAccount:
         self.owner = owner
         self.balance = balance
 
-    def deposit(self, amount):
-        self.balance += amount
+class AccountSaver():
+    @staticmethod
+    def save_to_db(account):
+        with open ('db.txt', 'a') as file:
+            file.writelines(f'{account.owner}:{account.balance}\n')
 
-    def withdraw(self, amount):
-        self.balance -= amount
+class BalanceManager():
+    @staticmethod
+    def deposit(account, amount=0):
+        account.balance += amount
 
-    def print_statement(self):
-        print(f"{self.owner}: {self.balance}")
+    @staticmethod
+    def withdraw(account, amount=0):
+        account.balance -= amount
 
-    def save_to_db(self):
-        pass
+class ShowInfo():
+    @staticmethod
+    def print_statement(account):
+        print(f"{account.owner}: {account.balance}")
+
+class AccountManager:
+    def __init__(self):
+        self.info = ShowInfo()
+        self.change = BalanceManager()
+        self.save_to_db = AccountSaver()
+
+acc1 = BankAccount('Bob Smith', 100)
+acc2 = BankAccount('Alice Goodenough', 150)
+
+account_manager = AccountManager()
+account_manager.info.print_statement(acc1)
+account_manager.change.deposit(acc1, 5)
+account_manager.info.print_statement(acc1)
+
+account_manager.change.withdraw(acc2, 10)
+account_manager.info.print_statement(acc2)
+
+account_manager.save_to_db.save_to_db(acc1)
+account_manager.save_to_db.save_to_db(acc2)
