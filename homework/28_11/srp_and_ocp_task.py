6f1cac1b3bf99ae0df413ea8d3f49b5e2c371286
netwgl <netwgl@gmail.com>
2025-12-07 20:09:06 +0100

    srp_and_ocp task

--- homework/28_11/srp_and_ocp_task.py
+++ homework/28_11/srp_and_ocp_task.py
@@ -21,53 +21,66 @@
 # PaymentService должен принимать любой PaymentMethod и работать без изменений при добавлении новых способов оплаты.
 
 from abc import ABC, abstractmethod
-from dataclasses import field, dataclass
 import datetime
+from time import sleep
 
 class PaymentMethod(ABC):
+    def __init__(self, holder: str, provider: str):
+        self.holder = holder
+        self.provider = provider
+        
     @abstractmethod
-    def pay(self, amount):
+    def pay(self, amount: float):
         raise NotImplementedError
     
 class CardPayment(PaymentMethod):
-    def __init__(self, card_holder, card_name):
-        self.card_holder = card_holder
-        self.card_name = card_name
+    def __init__(self, holder: str, provider: str):
+        super().__init__(holder, provider)
 
     def pay(self, amount):
-        print(f'{amount} paid by Card {self.card_name}: {self.card_holder}')
+        print(f'{amount} paid by Card {self.provider}: {self.holder}')
 
 class BankTransferPayment(PaymentMethod):
-    def __init__(self, account_holder, bank_name):
-        self.account_holder = account_holder
-        self.bank_name = bank_name
+    def __init__(self, holder: str, provider: str):
+        super().__init__(holder, provider)
 
-    def pay(self, amount):
-        print(f'{amount} paid by Bank Transfer {self.bank_name}: {self.account_holder}')
-
+    def pay(self, amount: float):
+        print(f'{amount} paid by Bank Transfer {self.provider}: {self.holder}')
 
 class PaymentLogger:
     @staticmethod    
-    def record_payment(payment_data):
-        with open('paymenet_log.txt', 'a') as file:
-            print(f'{datetime.time}:{payment_data.amount}')
-            file.writelines(f'{datetime.time}:{payment_data.amount}')
+    def record_payment(payment_method: PaymentMethod, amount: float, transaction_time: str):
+        with open(f'paymenet_log_{transaction_time}.txt', 'a') as file:
+            file.writelines(f'{transaction_time}:{payment_method.holder}:{payment_method.provider}:{amount}\n')
 
 class ReceiptGenerator:
     @staticmethod
-    def generate_receipt(payment_data):
-        print(f'Receipt:{datetime.time}:{payment_data.amount}:')
-        
+    def generate_receipt(payment_method: PaymentMethod, amount: float, transaction_time: str):
+        print(f'=> Receipt\nTime: {transaction_time}\nAmount: {amount}\nHolder: {payment_method.holder}\nProvider: {payment_method.provider}')
 
-class PaymentService(ReceiptGenerator, PaymentLogger):
-    @staticmethod
-    def pay(payment_data):
-        payment_data.pay(payment_data.amount)
-
-service = PaymentService()
+class PaymentService():
+    def __init__(self, payment_method: PaymentMethod, amount: float, receipt: ReceiptGenerator, logger: PaymentLogger, ):
+        self.logger = logger
+        self.receipt = receipt
+        self.payment_method = payment_method
+        self.amount = amount
+        self.transaction_time = None
+    
+    def pay(self):
+        self.transaction_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        self.payment_method.pay(self.amount)
+        self.receipt.generate_receipt(self.payment_method, self.amount, self.transaction_time)
+        self.logger.record_payment(self.payment_method, self.amount, self.transaction_time)
+    
+logger = PaymentLogger()
+receipt = ReceiptGenerator()
 card = CardPayment('Alice Superstar', 'Visa')
-card.pay(30)
 
+card_service = PaymentService(card, 30, receipt, logger)
+card_service.pay()
+
+sleep(15)
 transfer = BankTransferPayment('Bob Goodnight', 'Santander')
-transfer.pay(40)
-service(card)
+transfer_service = PaymentService(transfer, 50, receipt, logger)
+transfer_service.pay()
+
