6bd4122bff0ae2ca256584e2f10931b3432f6e15
netwgl <netwgl@gmail.com>
2025-12-15 21:54:07 +0100

    Comprehensions

--- homework/12_12/task_12_12.py
+++ homework/12_12/task_12_12.py
@@ -1,4 +1,4 @@
-#Есть список серверов:
+# Есть список серверов:
 # Функции, которые нужно написать:
 #   Функция для классификации нагрузки сервера
 #   Принимает количество запросов (requests)
@@ -7,14 +7,30 @@
 #   >500 and <1000 - medium
 #   <500 - low
 
+def load(requests: int):
+    if requests > 1000:
+        return 'high'
+    elif requests < 500:
+        return 'low'
+    return 'medium'
+
 #   Функция для проверки состояния сервера (healthy)
 #   Принимает словарь с информацией о сервере
 #   Возвращает True, если сервер онлайн и количество ошибок небольшое (<10), иначе False
 
+def healthy(info: dict):
+    if info['status'] == 'online' and info['errors'] < 10:
+        return True
+    return False
+
 # Задачи (выполнять через comprehensions)
 
-# Создать список строк вида "server-1: high load", используя функцию классификации нагрузки. Учитывать только online-серверы.
-# Создать словарь, где ключ — имя сервера, а значение — результат функции проверки состояния. Учитывать только online-серверы.
+# Создать список строк вида "server-1: high load", используя функцию классификации нагрузки. 
+# Учитывать только online-серверы.
+
+# Создать словарь, где ключ — имя сервера, а значение — результат функции проверки состояния. 
+# Учитывать только online-серверы.
+
 # Создать множество регионов, где есть нездоровые серверы.
 # Создать словарь, где ключ — регион, а значение — список серверов с высокой нагрузкой (high load).
 
@@ -25,3 +41,15 @@ servers = [
     {"name": "server-4", "status": "online", "requests": 400, "errors": 2, "region": "ASIA"},
     {"name": "server-5", "status": "offline", "requests": 0, "errors": 0, "region": "EU"},
 ]
+
+result1 = [record["name"] +': high load' for record in servers if load(record['requests']) == 'high' and healthy(record)]
+print(result1)
+
+result2 = {record["name"]:healthy(record) for record in servers if healthy(record)}
+print(result2)
+
+result3 = set(record["region"] for record in servers if not healthy(record))
+print(result3)
+
+result4 = {record["region"]:[item["name"] for item in servers if load(item["requests"]) == 'high' and item["region"] == record["region"]] for record in servers}
+print(result4)
