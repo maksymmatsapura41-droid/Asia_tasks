758373b4698d8bf3a80ac6c2c3c702de585b35ed
netwgl <netwgl@gmail.com>
2026-01-12 21:04:56 +0100

    json_task, retry_task

--- homework/04_01/retry_task.py
+++ homework/04_01/retry_task.py
@@ -1,11 +1,27 @@
 '''
-Напишите декоратор retry(times), который повторяет выполнение функции при возникновении исключения.
+Напишите декоратор retry(times), который повторяет выполнение 
+функции при возникновении исключения.
 times - количество попыток.
 Если все попытки неудачны, декоратор выбрасывает последнее исключение.
 Реализуйте возможность использовать декоратор без передачи аргументов.'''
 
+import functools
 
-# @retry(times=3)
+def retry(times=3):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            for i in range(1, times + 1):
+                try:
+                    return func(*args, **kwargs)
+                except Exception as e:
+                    print(f'The error happend, retrying {i} of {times}')
+                    func_error = e
+            return func_error
+        return wrapper
+    return decorator
+
+@retry(times=3)
 def risky_action():
     import random
     if random.random() < 0.7:
