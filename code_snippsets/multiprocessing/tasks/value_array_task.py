c164972e1e51560a16f7d2b9f2319d5675aa6a38
netwgl <netwgl@gmail.com>
2026-01-23 16:37:48 +0100

    value array task done

--- code_snippsets/multiprocessing/tasks/value_array_task.py
+++ code_snippsets/multiprocessing/tasks/value_array_task.py
@@ -12,3 +12,36 @@
 
 import multiprocessing
 import time
+
+
+def writer(array_to_change, flag):
+    print("Start array changing")
+    for i in range(len(array_to_change)):
+        
+        array_to_change[i] = i + 1
+        time.sleep(0.2)
+    flag.value = True
+    print('Finish array changing')
+    
+
+def reader(my_array, flag):
+    print("Start reading")
+    while not flag.value:
+        time.sleep(0.2)
+    print(sum(my_array))
+    
+
+
+if __name__ == "__main__":
+    my_array = multiprocessing.Array('i', 10)
+    flag = multiprocessing.Value('b', False)
+    
+
+    p1 = multiprocessing.Process(target=writer, args=(my_array, flag))
+    p2 = multiprocessing.Process(target=reader, args=(my_array, flag))
+    
+    p2.start()
+    p1.start()
+
+    p1.join()
+    p2.join()
