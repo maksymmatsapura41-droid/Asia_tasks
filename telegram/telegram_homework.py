85cdd4f3b9ba43db572e48d7b7758496e7e3060a
netwgl <netwgl@gmail.com>
2026-03-15 22:16:46 +0100

    weather bot

--- telegram/telegram_homework.py
+++ telegram/telegram_homework.py
@@ -16,3 +16,61 @@
 # 4. Обработка API (Utils):
 # Вынести логику запроса к API в отдельную асинхронную функцию в файле utils.py.
 # Функция должна принимать название города и возвращать отформатированную строку или словарь с данными.
+
+from aiogram import Bot, Dispatcher, types, F
+from aiogram.filters import CommandStart
+import logging
+from aiohttp import web
+import asyncio
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.fsm.context import FSMContext
+
+token = ''
+
+my_bot = Bot(token=token)
+chat_id = 479413823
+
+my_dispatcher = Dispatcher()
+
+class WeatherStates(StatesGroup):
+    waiting_for_city = State()
+    confirming_details = State()
+
+@my_dispatcher.message(CommandStart())
+async def cmd_start(message: types.Message, state: FSMContext):
+    await message.answer(f'Hello {message.from_user.first_name}! Enter the city name...')
+    await state.set_state(WeatherStates.waiting_for_city)
+
+async def get_weather(request):
+    data = await request.json()
+    print(data)
+    weather = '+15'
+    await my_bot.send_message(chat_id, weather)
+
+@my_dispatcher.message(WeatherStates.waiting_for_city)
+async def process_city(message: types.Message, state: FSMContext):
+    city = message.text
+    
+    await message.answer(f"There is a nice wather in {city}")
+    await state.clear()
+
+@my_dispatcher.message(F.text)
+async def cmd_start2(message: types.Message):
+    await message.answer(f'Please repeat, {message.chat.id}')
+
+async def main():
+    logging.basicConfig(level=logging.INFO)
+    await my_dispatcher.start_polling(my_bot)
+    app = web.Application()
+    runner = web.AppRunner(app)
+    await runner.setup()
+    server_port = web.TCPSite(runner, '0.0.0.0', 8888)
+    await server_port.start()
+
+    try:
+        await asyncio.Event().wait()
+    finally:
+        await runner.cleanup()
+
+if __name__=='__main__':
+    asyncio.run(main())
