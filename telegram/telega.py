e97c6de945fe6f0fe4ba11abf2e8ffc436129a98
netwgl <netwgl@gmail.com>
2026-03-20 16:40:35 +0100

    Transform to uppercase

--- telegram/telega.py
+++ telegram/telega.py
@@ -10,12 +10,12 @@ from aiogram.utils.keyboard import ReplyKeyboardBuilder
 from aiogram.types import ReplyKeyboardRemove
 
 load_dotenv()
-token = os.getenv('token')
+TOKEN = os.getenv('token')
 GITHUB_TOKEN = os.getenv('github_api_token')
-repo_name = os.getenv('repo_name')
-repo_owner = os.getenv('repo_owner')
-my_bot = Bot(token=token)
-chat_id = 479413823
+REPO_NAME = os.getenv('repo_name')
+REPO_OWNER = os.getenv('repo_owner')
+MY_BOT = Bot(token=TOKEN)
+CHAT_ID = 479413823
 
 my_dispatcher = Dispatcher()
 
@@ -27,8 +27,8 @@ async def github_webhook_handler(request):
         conclusion = data['workflow_run']['conclusion']
         status = data['workflow_run']['status']
         if conclusion:
-            await my_bot.send_message(chat_id, f'{name}: {conclusion}')
-        await my_bot.send_message(chat_id, f'{name}: {status}')
+            await MY_BOT.send_message(CHAT_ID, f'{name}: {conclusion}')
+        await MY_BOT.send_message(CHAT_ID, f'{name}: {status}')
 
 def get_keyboard():
     keyboard = ReplyKeyboardBuilder()
@@ -44,7 +44,7 @@ async def cmd_start(message: types.Message):
 
 @my_dispatcher.message(F.text.lower()=='start')
 async def action_start(message: types.Message):
-    url = f'https://api.github.com/repos/{repo_owner}/{repo_name}/actions/workflows/blank.yml/dispatches'
+    url = f'https://api.github.com/repos/{REPO_OWNER}/{REPO_NAME}/actions/workflows/blank.yml/dispatches'
     headers = {
         "Authorization": f"token {GITHUB_TOKEN}",
         "Accept": "application/vnd.github.v3+json"
@@ -60,7 +60,7 @@ async def action_start(message: types.Message):
 
 @my_dispatcher.message(F.text.lower()=='status')
 async def action_status(message: types.Message):
-    url = f"https://api.github.com/repos/{repo_name}/{repo_owner}/actions/runs"
+    url = f"https://api.github.com/repos/{REPO_NAME}/{REPO_OWNER}/actions/runs"
     headers = {
     "Authorization": f"token {GITHUB_TOKEN}",
     "Accept": "application/vnd.github.v3+json"
@@ -69,7 +69,7 @@ async def action_status(message: types.Message):
     async with aiohttp.ClientSession() as session:
         async with session.get(url=url, headers=headers, params=params) as answer:
             result = await answer.json()
-            
+
 
 # @my_dispatcher.message(F.text)
 # async def cmd_start(message: types.Message):
@@ -83,7 +83,7 @@ async def main():
     await runner.setup()
     server_port = web.TCPSite(runner, '0.0.0.0', 8888)
     await server_port.start()
-    await my_dispatcher.start_polling(my_bot)
+    await my_dispatcher.start_polling(MY_BOT)
 
     try:
         await asyncio.Event().wait()
