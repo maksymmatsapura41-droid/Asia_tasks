e55ef26c1c3f4cfde955350e259bd1a90f6baa17
netwgl <netwgl@gmail.com>
2026-03-20 11:36:26 +0100

    Вынести логику запроса к API в отдельную асинхронную функцию в файле utils.py

--- telegram/telegram_homework.py
+++ telegram/telegram_homework.py
@@ -21,12 +21,12 @@ from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters import CommandStart
 import logging
 from aiohttp import web
-import aiohttp
 import asyncio
 from aiogram.fsm.state import StatesGroup, State
 from aiogram.fsm.context import FSMContext
 from dotenv import load_dotenv
 import os
+import utils
 
 load_dotenv()
 token = os.getenv('token')
@@ -50,24 +50,10 @@ async def process_sticker(message: types.Message, state: FSMContext):
     await message.answer("Красивый стикер! Но я понимаю только названия городов. Введи город:")
     await state.set_state(WeatherStates.waiting_for_city)
 
-async def get_weather(city):
-    units = 'metric'
-    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&APPID={openweathermap_api_key}&units={units}'
-    async with aiohttp.ClientSession() as session:
-        try:
-            async with session.get(url) as response:
-                json_data = await response.json()
-                if json_data['cod'] == '404':
-                    return f'К сожалению, я не нашел такой город {city}. Попробуйте еще раз'
-                else:
-                    return f"В городе {city} сейчас {json_data['main']['temp']}°C {json_data['weather'][0]['description']}"
-        except Exception as e:
-            return city, str(e)
-
 @my_dispatcher.message(WeatherStates.waiting_for_city)
 async def process_city(message: types.Message, state: FSMContext):
     city = message.text
-    result = await get_weather(city)
+    result = await utils.get_weather(city, openweathermap_api_key)
     await message.answer(result)
     await state.clear()
 
