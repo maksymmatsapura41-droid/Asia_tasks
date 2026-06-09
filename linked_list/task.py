893a3c8ee3226873607f7b850f13523f2eaffb82
netwgl <netwgl@gmail.com>
2026-06-09 17:01:58 +0200

    classwork done

--- linked_list/task.py
+++ linked_list/task.py
@@ -2,32 +2,60 @@
 # Односвязный список реализовав два класса: Node (узел) и LinkedList (управляющий класс).
 
 class Node:
-    pass
-
+    def __init__(self, value):
+        self.next = None
+        self.value = value
 
 class LinkedList:
     def __init__(self):
-        pass
+        self.head = None
 
     def append(self, value):
         """Добавить элемент в конец списка"""
-        pass
+        new_node = Node(value)
+        if not self.head:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
 
     def prepend(self, value):
         """Добавить элемент в начало списка (новый head)"""
-        pass
+        new_node = Node(value)
+        new_node.next = self.head
+        self.head = new_node
 
     def delete(self, value):
         """Удалить первое вхождение узла с указанным value"""
-        pass
+        if self.head.value == value:
+            self.head = self.head.next
+            return
+        
+        current = self.head
+        while current.next and current.next.value != value:
+            current = current.next
+        if current.next:
+            current.next = current.next.next 
 
     def find(self, value) -> bool:
         """Вернуть True, если элемент есть в списке, иначе False"""
-        pass
+        current = self.head
+        while current:
+            if current.value == value:
+                return True
+            current = current.next
+        return False
 
     def display(self):
         """Вывести список в формате: 1 -> 2 -> 3 -> None"""
-        pass
+        values = []
+        current = self.head
+        while current:
+            values.append(str(current.value))
+            current = current.next
+        print(" -> ".join(values) + " -> None" if values else 'None')
 
 
 if __name__ == "__main__":
