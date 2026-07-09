e767676eeb305179caec1cec3fe3e645c6899c6a
mmatsapura <mmatsapura2905@gmail.com>
2026-07-09 11:28:03 +0300

    Add tasks

---
+++ code_snippsets/recursion/tasks.py
@@ -0,0 +1,21 @@
+"""
+1.Условие: Напиши рекурсивную функцию, которая выводит числа от 1 до n, а затем от n до 1. Использовать циклы нельзя.
+mirror_print(3)
+# 3 2 1 1 2 3
+
+2. Условие: Напиши рекурсивную функцию, которая возвращает количество гласных букв (a, e, i, o, u) в строке.
+count_vowels("apple")
+# 2
+
+3. Проверка на палиндром
+Условие: Напишите рекурсивную функцию, которая определяет, является ли строка палиндромом.
+is_palindrome("radar")
+# True
+
+is_palindrome("hello")
+# False
+----------------------------------------
+https://leetcode.com/problems/reverse-linked-list/description/
+
+https://leetcode.com/problems/remove-linked-list-elements/description/
+"""
